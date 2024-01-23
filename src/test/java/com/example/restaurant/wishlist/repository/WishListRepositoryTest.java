758254a7227025149fdbcd5eb4b6a063cfbd4329
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-01-23 23:17:22 +0900

    test 코드

--- src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
+++ src/test/java/com/example/restaurant/wishlist/repository/WishListRepositoryTest.java
@@ -39,16 +39,49 @@ public class WishListRepositoryTest {
 
     @Test
     public void findByIdTest() {
+        WishListEntity wishListEntity = createWishList();
+        wishListRepository.save(wishListEntity);
 
+        var expected = wishListRepository.findById(1);
+
+        Assertions.assertEquals(true, expected.isPresent());
+        Assertions.assertEquals(1, expected.get().getIndex());
+    }
+
+    @Test
+    public void updateTest() {
+        WishListEntity wishListEntity = createWishList();
+        WishListEntity expected = wishListRepository.save(wishListEntity);
+
+        expected.setTitle("update test");
+        WishListEntity savEntity = wishListRepository.save(expected);
+
+        Assertions.assertEquals("update test", savEntity.getTitle());
+        Assertions.assertEquals(1, wishListRepository.listAll().size());
     }
 
     @Test
     public void deleteTest() {
+        WishListEntity wishListEntity = createWishList();
+        wishListRepository.save(wishListEntity);
 
+        wishListRepository.deleteById(1);
+
+        int count = wishListRepository.listAll().size();
+
+        Assertions.assertEquals(0, count);
     }
     
     @Test
     public void listAllTest() {
+        WishListEntity wishListEntity1 = createWishList();
+        wishListRepository.save(wishListEntity1);
+        WishListEntity wishListEntity2 = createWishList();
+        wishListRepository.save(wishListEntity2);
+
+        int count = wishListRepository.listAll().size();
+
+        Assertions.assertEquals(2, count);
 
     }
 }
